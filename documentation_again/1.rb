40ca7647eac65e7195d2602b25529c88835f6809
westonludeke <westonludeke@gmail.com>
2020-02-05 12:34:53 -0600

    first upload

---
+++ documentation_again/1.rb
@@ -0,0 +1,3 @@
+# Locate the ruby documentation for methods File::path and File#path. How are they different?
+
+# My answer: Methods denoted by :: are considered 'class' methods, whereas methods denoted by # are considered 'instance' methods.
---
+++ documentation_again/2.rb
@@ -0,0 +1,15 @@
+# Assume you have the following code:
+
+require 'date'
+
+puts Date.new
+puts Date.new(2016)
+puts Date.new(2016, 5)
+puts Date.new(2016, 5, 13)
+
+# What will each of the 4 puts statements print?
+
+# -4712-01-01
+# 2016-01-01
+# 2016-05-01
+# 2016-05-13
