962faf74e2c107ba7b0e886dc4717324bd7bceff
westonludeke <westonludeke@gmail.com>
2020-02-06 11:44:10 -0600

    Finished 'Documentation Again'

---
+++ documentation_again/8.rb
@@ -0,0 +1,8 @@
+# Included Modules
+
+# Use irb to run the following code:
+a = [5, 9, 3, 11]
+puts a.min
+
+# Find the documentation for the #min method and change the above code to print the two smallest values in the Array.
+
