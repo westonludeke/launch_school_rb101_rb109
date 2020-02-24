b2ff13baef1e5398b3d0c53c0f8aa7eef59531be
westonludeke <westonludeke@gmail.com>
2020-02-24 13:28:20 -0600

    Finished #10 of Easy 1

---
+++ 1_easy/10.rb
@@ -0,0 +1,15 @@
+# What's my bonus?
+
+# Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+
+def calculate_bonus(num, goal)
+  if goal == true
+    return (num / 2) 
+  else
+    return 0
+  end
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
