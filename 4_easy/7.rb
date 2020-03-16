bceccdda74e5ff2be03c39c014476ee3c2cacf79
westonludeke <westonludeke@gmail.com>
2020-03-16 16:15:45 -0500

    Finished #7

--- 4_easy/7.rb
+++ 4_easy/7.rb
@@ -1,14 +1,25 @@
 # Convert a String to a Number!
 
-def string_to_integer(num)
-  #p num
+def string_to_integer(string)
   digits = {"1" => 1, "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "0" => 0}
+  # This puts each character of the String into an Array, where each character is a separate String value
+  # arr = num.chars
+  # # This converts each String value of the array to an integer, pulling from the Digits hash
+  # arr.map! { |key, value| digits.fetch(key, value) }
+  # # This pulls each integer value out of the array and returns it separately
+  # arr.select! {|x| x}
+  # puts arr
 
-  arr = num.chars
-  arr.map! { |key, value| digits.fetch(key, value) }
-  #p arr
+  # I failed with my code above to solve the problem.
+  # ...but I found this good solution via StackOverflow: https://stackoverflow.com/a/40755501/5137112
+  total = 0
+  num_array = string.split("").reverse
+  num_array.length.times do |i|
+   num_value = digits[num_array[i]]
+   num_value_base_ten = num_value * (10**i)
+   total += num_value_base_ten
+ end
+ return total
 end
 
-string_to_integer('1234')
-# string_to_integer('4321') == 4321
-# string_to_integer('570') == 570
+p string_to_integer('1234')
