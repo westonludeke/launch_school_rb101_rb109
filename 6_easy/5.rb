802e20730ec1ecb4822b12ac8ad9194cec590568
westonludeke <westonludeke@gmail.com>
2020-03-25 14:24:19 -0500

    Finished #5

---
+++ 6_easy/5.rb
@@ -0,0 +1,23 @@
+# Reversed Arrays (Part 2)
+# https://launchschool.com/exercises/577016f3
+
+# Write a method that takes an Array, and returns a new Array with the elements of the original list in reverse order. Do not modify the original list.
+
+#You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
+
+def reverse(array)
+    array.each_with_index.map do |value, index|
+        array[array.count-index-1]
+    end
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => truep 
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
