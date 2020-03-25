6ade777a467172ab397467f30f38fbcd2e9973f9
westonludeke <westonludeke@gmail.com>
2020-03-25 18:14:45 -0500

    Finished #9

---
+++ 6_easy/9.rb
@@ -0,0 +1,14 @@
+# Does My List Include This?
+# https://launchschool.com/exercises/860cfef1
+
+# Write a method named include? that takes an Array and a search value as arguments. This method should return true if the search value is in the array, false if it is not. You may not use the Array#include? method in your solution.
+
+def include?(arr, search)
+  arr.any? { |word| word == search }
+end
+
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
