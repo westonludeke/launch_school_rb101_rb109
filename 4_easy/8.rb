7d9549dea895f3ff8363f425f2b44f1de1fc9ecf
westonludeke <westonludeke@gmail.com>
2020-03-25 19:16:53 -0500

    Easy 7

---
+++ 4_easy/8.rb
@@ -0,0 +1,17 @@
+
+
+DIGITS = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}
+
+def string_to_signed_integer(string)
+  digits = string.chars.map { |char| DIGITS[char] }
+
+  value = 0
+  digits.each { |digit| value = 10 * value + digit }
+  value
+end
+
+#p string_to_signed_integer("4321")
+p string_to_signed_integer("-570")
