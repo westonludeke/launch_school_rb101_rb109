406d549f51f38b7c2ee97469d8ce927b0248d35e
westonludeke <westonludeke@gmail.com>
2020-03-25 19:20:25 -0500

    Rename folder

---
+++ 7easy/1.rb
@@ -0,0 +1,11 @@
+# Combine Two Lists
+
+# Write a method that combines two Arrays passed in as arguments, and returns a new Array that contains all elements from both Array arguments, with the elements taken in alternation.
+
+# You may assume that both input Arrays are non-empty, and that they have the same number of elements.
+
+def interleave(arr, arr2)
+  arr.zip(arr2).flatten.compact
+end
+
+interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
