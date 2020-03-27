e807441fffb13857fc284cd9ad622c18f78ccac6
westonludeke <westonludeke@gmail.com>
2020-03-26 20:07:06 -0500

    Finished #8

--- 7_easy/8.rb
+++ 7_easy/8.rb
@@ -2,11 +2,16 @@
 
 # Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
 
-def multiply_lists(arr,arr_two)
+def multiply_list(arr,arr_two)
+  i = 0
+  new_arr = []
   arr.each_with_index do |x, i|
-    x * arr_two[i]
+    product = x * arr_two[i]
+    i += 1
+    new_arr << product
   end
+  new_arr
 end
 
-p multiply_list([3, 5, 7], [9, 10, 11]) #== [27, 50, 77]
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
 
