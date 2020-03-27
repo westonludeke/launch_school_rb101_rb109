c07cbab538b4dd34800b7572d9afcd8753f9df99
westonludeke <westonludeke@gmail.com>
2020-03-26 20:27:50 -0500

    Finished #10 & Easy 7

---
+++ 7_easy/10.rb
@@ -0,0 +1,16 @@
+# The End Is Near But Not Here
+
+# Write a method that returns the next to last word in the String passed to it as an argument.
+
+# Words are any sequence of non-blank characters.
+
+# You may assume that the input String will always contain at least two words.
+
+def penultimate(str)
+  arr = []
+  arr = str.split(" ")
+  arr[-2]
+end
+
+p penultimate('last word') == 'last'
+p penultimate('Launch School is great!') == 'is'
