ba8a709d7da209f92cf4971e620b3a89f175eb79
westonludeke <westonludeke@gmail.com>
2020-03-30 16:11:24 -0500

    Finished #8

---
+++ 9_easy/8.rb
@@ -0,0 +1,22 @@
+# Sequence Count
+
+# Create a method that takes two integers as arguments. The first argument is a count, and the second is the first number of a sequence that your method will create. The method should return an Array that contains the same number of elements as the count argument, while the values of each element will be multiples of the starting number.
+
+# You may assume that the count argument will always have a value of 0 or greater, while the starting number can be any integer value. If the count is 0, an empty list should be returned.
+
+def sequence(arr_leng, num)
+  arr = []
+  i = 0
+
+  until arr.length == arr_leng
+    i += 1
+    arr << num * i
+  end
+  arr
+end
+
+
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
