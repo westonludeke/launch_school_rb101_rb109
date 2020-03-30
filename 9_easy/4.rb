bef8fa66b8f90b1056e089993808407b6ea3e744
westonludeke <westonludeke@gmail.com>
2020-03-30 11:47:51 -0500

    Finished #4

---
+++ 9_easy/4.rb
@@ -0,0 +1,13 @@
+# Counting Up
+
+# Write a method that takes an integer argument, and returns an Array of all integers, in sequence, between 1 and the argument.
+
+# You may assume that the argument will always be a valid integer that is greater than 0.
+
+def sequence(num)
+ (1..num).to_a
+end
+
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
